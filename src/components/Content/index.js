84dc8c8e23948a7aa72e4fe91929b22a75b24dd2
dthabata <dthabatita@gmail.com>
2024-03-27 11:53:44 -0300

    feat: PokemonDetails Modal in progress

--- src/components/Content/index.js
+++ src/components/Content/index.js
@@ -1,13 +1,13 @@
-import React from 'react';
-import { Input, Space, Layout } from 'antd';
+import React, { useState } from 'react';
+import { Input, Space, Layout, Button } from 'antd';
+import PokemonDetails from '../Modal';
 import poke1 from '../../assets/pikachu.png';
 import './styles.css';
 
 const { Search } = Input;
+const { Content } = Layout;
 
 const onSearch = (value, _e, info) => console.log(info?.source, value);
-
-const { Content } = Layout;
 
 const contentStyle = {
     backgroundColor: '#fff',
@@ -17,6 +17,16 @@ const contentStyle = {
 };
 
 const AppContent = () => {
+    const [modalOpen, setModalOpen] = useState(false);
+
+    const handleOpenModal = () => {
+        setModalOpen(true);
+    };
+
+    const handleCloseModal = () => {
+        setModalOpen(false);
+    };
+
     return (
         <main>
             <Content style={contentStyle}>
@@ -69,7 +79,10 @@ const AppContent = () => {
                         <img src={poke1} alt="" />
                         <h3>Nome do Pokémon</h3>
                         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-                        <a href="www.google.com">Saiba mais</a>
+                        <a href="#" onClick={handleOpenModal}>
+                            Abrir Modal
+                        </a>
+                        <PokemonDetails nome="false" />
                     </div>
             </section>
         </main>
