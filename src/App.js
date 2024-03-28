382f7827e4a7c966405204c8df56fedbc526c785
dthabata <dthabatita@gmail.com>
2024-03-28 16:34:36 -0300

    feat: adjusting CSS for App.js

--- src/App.js
+++ src/App.js
@@ -5,24 +5,12 @@ import AppContent from './components/Content';
 
 const { Footer } = Layout;
 
-const footerStyle = {
-  backgroundColor: '#fff',
-  marginTop: '68px',
-};
-
-const layoutStyle = {
-  backgroundColor: '#fff',
-  overflow: 'hidden',
-  width: 'calc(50% - 8px)',
-  maxWidth: 'calc(100% - 0px)',
-};
-
 const App = () => (
   <Flex gap="middle" wrap="wrap">
-    <Layout style={layoutStyle}>
+    <Layout className="layout">
       <AppHeader />
       <AppContent />
-      <Footer style={footerStyle} />
+      <Footer className="footer" />
     </Layout>
   </Flex>
 );
