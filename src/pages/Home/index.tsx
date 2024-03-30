0e6069f37be2aab05247bd1db6f694e99316393b
dthabata <dthabatita@gmail.com>
2024-03-30 17:02:34 -0300

    feat: Home to TS

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Layout, Flex } from 'antd';
 import AppHeader from '../../components/Header';
+// TODO: Content to TS
+// @ts-ignore
 import AppContent from '../../components/Content';
 
 const { Footer } = Layout;
 
-const Home = () => (
+const Home: React.FC = () => (
     <Flex gap="middle" wrap="wrap">
         <Layout className="layout">
         <AppHeader />
