07b087fb8f4cdf0f3ebfecf1a695d1e97fb7bc3a
dthabata <dthabatita@gmail.com>
2024-03-30 13:14:54 -0300

    StyledComponents for StatsPokemon

--- src/components/StatsPokemon/index.js
+++ src/components/StatsPokemon/index.js
@@ -1,12 +1,20 @@
 import React from 'react';
+import {
+    StyledSection,
+    StyledH4,
+    StyledText,
+    StyledContent,
+    StyledSkills,
+    StyledImage,
+    StyledNumbers,
+} from './styles.js';
 import { Flex, Progress } from 'antd';
 import badge1 from '../../assets/badge-1.png';
-import './styles.css';
 
 const StatsPokemon = () => {
     return (
-        <section className="stats-info">
-            <h4>Base Stats</h4>
+        <StyledSection className="stats-info">
+            <StyledH4>Base Stats</StyledH4>
             <div className="stats-progress">
                 <Flex gap="small" vertical>
                     <Progress percent={30} />
@@ -16,56 +24,22 @@ const StatsPokemon = () => {
                     <Progress percent={50} showInfo={false} />
                 </Flex>
             </div>
-            <div className="stats-info-text">
+            <StyledText>
                 The ranges shown on the right are for a level 100 pokemon. Maximum values are based on a beneficial nature, 252 EVs, 31 IVs; minimum values are based on a hindering nature, 0 EVs, 0 IVs.
-            </div>
-            <h4>Type Defenses</h4>
-            <div className="stats-info-content">
-            The effectiveness of each type on Bulbasaur.
-            </div>
-
-            <div class="stats-info-skills">
-                <img className="stats-img" src={badge1} alt="" />
-                <img className="stats-img" src={badge1} alt="" />
-                <img className="stats-img" src={badge1} alt="" />
-                <img className="stats-img" src={badge1} alt="" />
-                <img className="stats-img" src={badge1} alt="" />
-                <img className="stats-img" src={badge1} alt="" />
-                <img className="stats-img" src={badge1} alt="" />
-                <img className="stats-img" src={badge1} alt="" />
-                <img className="stats-img" src={badge1} alt="" />
-
-                <p class="random-numbers">2</p>
-                <p class="random-numbers">3</p>
-                <p class="random-numbers">6</p>
-                <p class="random-numbers">2</p>
-                <p class="random-numbers">0</p>
-                <p class="random-numbers">1</p>
-                <p class="random-numbers">0</p>
-                <p class="random-numbers">4</p>
-                <p class="random-numbers">5</p>
-
-                <img className="stats-img" src={badge1} alt="" />
-                <img className="stats-img" src={badge1} alt="" />
-                <img className="stats-img" src={badge1} alt="" />
-                <img className="stats-img" src={badge1} alt="" />
-                <img className="stats-img" src={badge1} alt="" />
-                <img className="stats-img" src={badge1} alt="" />
-                <img className="stats-img" src={badge1} alt="" />
-                <img className="stats-img" src={badge1} alt="" />
-                <img className="stats-img" src={badge1} alt="" />
-
-                <p class="random-numbers">3</p>
-                <p class="random-numbers">0</p>
-                <p class="random-numbers">0</p>
-                <p class="random-numbers">1</p>
-                <p class="random-numbers">2</p>
-                <p class="random-numbers">3</p>
-                <p class="random-numbers">4</p>
-                <p class="random-numbers">4</p>
-                <p class="random-numbers">5</p>
-            </div>
-        </section>
+            </StyledText>
+            <StyledH4>Type Defenses</StyledH4>
+            <StyledContent>
+                The effectiveness of each type on Bulbasaur.
+            </StyledContent>
+            <StyledSkills>
+                {[...Array(18)].map((_, index) => (
+                    <React.Fragment key={index}>
+                        <StyledImage className="stats-img" src={badge1} alt="" />
+                        <StyledNumbers className="random-numbers">{Math.floor(Math.random() * 6)}</StyledNumbers>
+                    </React.Fragment>
+                ))}
+            </StyledSkills>
+        </StyledSection>
     );
 }
 
--- src/components/StatsPokemon/styles.js
+++ src/components/StatsPokemon/styles.js
@@ -1,13 +1,15 @@
-.stats-info {
+import styled from 'styled-components';
+
+export const StyledSection = styled.section`
     display: flex;
     flex-direction: column;
     flex-wrap: wrap;
     justify-content: center;
     text-align: center;
     align-items: left;
-}
+`;
 
-h4 {
+export const StyledH4 = styled.h4`
     color: #1677ff;
     font-size: 16px;
     line-height: 19.09px;
@@ -16,38 +18,38 @@ h4 {
     justify-content: center;
     text-align: justify;
     margin-top: 10px;
-}
+`;
 
-.stats-info-text {
+export const StyledText = styled.div`
     text-align: justify;
     color: rgba(116, 116, 118, 1);
     font-size: 16px;
     line-height: 19.09px;
     margin-top: 20px;
     margin-bottom: 20px;
-}
+`;
 
-.stats-info-content {
+export const StyledContent = styled.div`
     text-align: justify;
     color: rgba(116, 116, 118, 1);
     font-size: 16px;
     line-height: 19.09px;
-}
+`;
 
-.stats-info-skills {
+export const StyledSkills = styled.div`
     display: grid;
     grid-template-columns: repeat(9, 1fr);
     grid-template-rows: repeat(4, auto);
     gap: 2px 0;
     margin-top: 25px;
-}
+`;
 
-.stats-img {
+export const StyledImage = styled.img`
     margin-top: 20px;
     justify-self: left;
-}
+`;
 
-.random-numbers {
+export const StyledNumbers = styled.p`
     padding-left: 7px;
     justify-self: left;
-}
+`;
